a7d0946f905da023222637f03e913e8e0456e961
bhavadharanii <bhavadharan.s@fundsindia.com>
2023-01-03 10:58:41 +0530

    CollectionView

--- SampleCollectionView/ViewController.swift
+++ SampleCollectionView/ViewController.swift
@@ -28,19 +28,95 @@ class ViewController: UIViewController {
     var collectionView: UICollectionView?
     var dataValues: [String] = ["AC","-/+","%","/","7","8","9","*","4","5","6","-","1","2","3","+","0",".","="]
     var newValue: String?
-    
-    
+    var characterArray:[Character] =  []
+
+    var firstNumber: Int?
+    var secondNumber: Int?
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+
     //find the operatar
+
     func doRespectiveOperation() {
-        
-        print(" = ")
+        characterArray = newValue?.charactersArray ?? []
+        var output : Int?
+        for (_index, value) in characterArray.enumerated() {
+            if value == "+"
+            {
+                commonFunction(index: _index)
+                print("Add :\( doAdd(firstValue: firstNumber ?? 0, secondValue: secondNumber ?? 0))")
+            }
+            else if value == "-"
+            {
+                commonFunction(index: _index)
+                print("Sub :\(doSubtract(firstValue: firstNumber ?? 0, secondValue: secondNumber ?? 0))")
+            }
+            else if value == "*"
+            {
+               commonFunction(index: _index)
+               print("Muultiply :\(doMultiply(firstValue: firstNumber ?? 0, secondValue: secondNumber ?? 0))")
+                
+            }
+            else if value == "/"
+            {
+                commonFunction(index: _index)
+               print("Divide :\(doDivide(firstValue: firstNumber ?? 0, secondValue: secondNumber ?? 0))")
+                
+            }
+            else if value == "%"
+            {
+                commonFunction(index: _index)
+                print("Modulo :\(doModulo(firstValue: firstNumber ?? 0, secondValue: secondNumber ?? 0))")
+                
+            }
+        }
+        func doAdd(firstValue: Int , secondValue: Int ) -> Int {
+           output = firstValue + secondValue
+            return output ?? 0
+          
+        }
+        func doSubtract(firstValue: Int , secondValue: Int ) -> Int {
+            
+          output = firstValue - secondValue
+            return output ?? 0
+            
+        }
+        func doDivide(firstValue: Int , secondValue: Int ) -> Int {
+            output = firstValue / secondValue
+            return output ?? 0
+        }
+        func doMultiply(firstValue: Int , secondValue: Int ) -> Int {
+           output = firstValue * secondValue
+            return output ?? 0
+        }
+        func doModulo(firstValue: Int , secondValue: Int ) -> Int {
+            
+            output =  firstValue % secondValue
+            return output ?? 0
+        }
     }
     
+    func commonFunction(index: Int) {
+        print(characterArray[index])
+        firstNumber = getNumber(startIndex: 0, endIndex: index-1)
+        secondNumber = getNumber(startIndex: index+1, endIndex: characterArray.count-1)
+    }
     
+    func getNumber(startIndex: Int , endIndex: Int) -> Int {
+        var number : String?
+        for i in startIndex...endIndex {
+            number = (number ?? "") + String(characterArray[i])
+            
+        }
+        print("number:\(number)")
+        return Int(number ?? "") ?? 0
+    }
+    
+    func clearAll() {
+        label2.text = ""
+    }
 }
 
 
@@ -91,21 +167,20 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource,U
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         newValue = label2.text
-        if let _newValue = newValue {
+        if let _newValue = newValue, dataValues[indexPath.row] != "=" {
             newValue = _newValue + self.dataValues[indexPath.row]
         }
         label2.text = newValue
         
-        if dataValues[indexPath.row] == "="
-        {
+        if dataValues[indexPath.row] == "=" {
             doRespectiveOperation()
+        } else if dataValues[indexPath.row] == "AC" {
+                clearAll()
         }
+
     }
     
-    
-    
-        
-    //verticalspacing for the cells
+//verticalspacing for the cells
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
     
         return 1
@@ -120,7 +195,17 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource,U
 }
 
 
+public extension String {
+    /// SwifterSwift: Array of characters of a string.
+    var charactersArray: [Character] {
+        return Array(self)
+    }
+}
 
-        
-       
 
+//func XXX{
+//    firstnuumberr=
+//    seccconod
+//    output=
+//
+//}
